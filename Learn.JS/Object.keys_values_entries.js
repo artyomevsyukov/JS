825800c84dca28a7be02a61651363f13f0a9658a
Artyom Evsyukov <artyom.evsyukov@gmail.com>
2024-01-29 15:21:25 +0300

    pc

---
+++ Learn.JS/Object.keys_values_entries.js
@@ -0,0 +1,37 @@
+// let salaries = {
+//     John: 100,
+//     Pete: 300,
+//     Mary: 250,
+// };
+
+// function sumSalaries(obj) {
+//     let sum = 0;
+//     for (let item of Object.values(obj)) {
+//         sum += item;
+//     }
+//     return sum;
+// }
+// function sumSalaries(obj) {
+//     let sum = Object.values(obj);
+//     // .reduce((sum, value) => (sum += value));
+
+//     return sum;
+// }
+
+// console.log(sumSalaries(salaries)); // 650
+
+// let user = {
+//     name: "John",
+//     age: 30,
+//     age2: 33,
+// };
+
+// function count(obj) {
+//     return Object.entries(obj).length;
+// }
+
+// console.log(count(user)); // 2
+
+
+git config --global user.name "Artyom Evsyukov"
+git config --global user.email artyom.evsyukov@gmail.com
