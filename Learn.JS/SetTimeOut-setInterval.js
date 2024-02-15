94fad7df5a0235a9a59811e67e9efa544b43d322
Artyom.Evsyukov <artyom.evsyukov@gmail.com>
2024-02-15 15:08:57 +0300

    work 15.02

--- Learn.JS/SetTimeOut-setInterval.js
+++ Learn.JS/SetTimeOut-setInterval.js
@@ -26,11 +26,171 @@
 // console.log(z);
 // // setTimeout(sayHi, 1000);
 
-function sum(a = 2, b = 3) {
-    return a + b;
-    return console.log(a + b);
+// function sum(a = 2, b = 3) {
+//     return console.log(a + b);
+// }
+// // let x = sum(2, 3);
+// // console.log(sum());
+// // setTimeout(sum, 1000);
+// let timer = setTimeout(sum, 2000, 2, 3);
+// clearTimeout(timer, 1000);
+
+// let timerId = setInterval(() => console.log("tick"), 1000);
+// setTimeout(() => {
+//     clearInterval(timerId), console.log("stop");
+// }, 5100);
+
+// let delay = 500;
+// let error347 = true;
+// let timerId = setTimeout(function request() {
+//     //код запроса на сервер
+//     console.log(delay);
+
+//     if (error347) {
+//         // увеличить интервал для следующего запроса
+//         if (delay <= 30000) delay *= 2;
+//     }
+
+//     timerId = setTimeout(request, delay);
+// }, delay);
+
+// let delay = 100;
+// let timer = setTimeout(function server() {
+//     console.log(delay);
+//     if (error347) {
+//         if (delay <= 1000) {
+//             delay *= 2;
+//         } else delay = 1200;
+//         timer = setTimeout(server, delay);
+//     }
+// }, delay);
+// ===================================================
+
+// let start = Date.now();
+// let times = [];
+
+// setTimeout(function run() {
+//     times.push(Date.now() - start); // запоминаем задержку от предыдущего вызова
+
+//     if (start + 100 < Date.now())
+//         console.log(times); // показываем задержку через 100 мс
+//     else setTimeout(run); // если нужно ещё запланировать
+// });
+
+// function sayHi() {
+//     console.log("Hi");
+
+//     // давайте посчитаем, сколько вызовов мы сделали
+//     sayHi.counter++;
+// }
+// sayHi.counter = 0; // начальное значение
+
+// sayHi(); // Hi
+// sayHi(); // Hi
+
+// console.log(`Вызвана ${sayHi.counter} раза`); // Вызвана 2 раза
+//=====================
+
+// function printNumbers(from, to) {
+//     let current = from;
+//     let timerId = setInterval(function () {
+//         console.log(current);
+//         if (current >= to) {
+//             clearInterval(timerId);
+//         }
+//         current++;
+//     }, 300);
+// }
+
+// printNumbers(3, 8);
+
+// function printNumbers(from, to) {
+//     let current = from;
+//     let timerId = setTimeout(function go() {
+//         console.log(current);
+
+//         if (current < to) {
+//             setTimeout(go, 300);
+//         }
+//         current++;
+//         // clearTimeout(timerId);
+//     }, 300);
+// }
+
+// printNumbers(3, 8);
+
+// function printNumbers(from, to) {
+//     let current = from;
+
+//     function go() {
+//         console.log(current);
+//         if (current >= to) {
+//             clearInterval(timerId);
+//         }
+//         current++;
+//     }
+//     let timerId = setInterval(go, 300);
+// }
+// printNumbers(3, 8);
+// go();
+
+// // ================================================
+// function printNumbers(from, to) {
+//     let current = from;
+
+//     function go() {
+//         console.log(current);
+//         if (current < to) {
+//             current++;
+//             setTimeout(go, 300);
+//         }
+//     }
+//     go();
+//     // setTimeout(go, 1000);
+// }
+
+// printNumbers(3, 8);
+
+// function printNumbers(from, to) {
+//     let current = from;
+
+//     function go() {
+//         console.log(current);
+//         if (current < to) {
+//             current++;
+//             setTimeout(go, 300); // вызываем снова через 300 миллисекунд
+//         }
+//     }
+
+//     go(); // вызываем функцию go сразу после определения
+// }
+
+// printNumbers(1, 15);
+
+// function printNumbers(from, to) {
+//     let current = from;
+
+//     function go() {
+//         console.log(current);
+//         if (current < to) {
+//             setTimeout(go, 1000);
+//         }
+//         current++;
+//     }
+//     setTimeout(go, 5000);
+// }
+
+// // использование:
+// printNumbers(5, 10);
+
+function secondItem() {
+    setTimeout(() => console.log("5-hello"), 50);
+
+    console.log("1-Max");
+    console.log("2-Den");
+    setTimeout(() => console.log("4-hello"), 20);
 }
-// let x = sum(2, 3);
-// console.log(sum());
-setTimeout(sum, 1000);
-// setTimeout(sum(), 1000, 2, 3);
+secondItem();
+
+setTimeout(() => console.log("444444"), 45);
+console.log("3-Ken");
