28302fbfd8b9cdd27780dc0f5d1a24e48d2762f3
Artyom.Evsyukov <artyom.evsyukov@gmail.com>
2024-02-28 14:47:17 +0300

    work 28.02/2

--- Learn.JS/code-basics.com copy.js
+++ Learn.JS/code-basics.com copy.js
@@ -196,3 +196,6 @@
 // console.log(encrypt("attack")); // taatkc
 
 // console.log(encrypt("go!")); // og!
+
+const text = "When \t\n you play a \t\n game of thrones you win or you die.";
+console.log(text.slice(5, 15).trim().length);
