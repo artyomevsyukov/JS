dce38d162573e6b6e07a579e79a2833e2156678f
Artyom.Evsyukov <artyom.evsyukov@gmail.com>
2024-03-04 11:37:22 +0300

    work 04.03

--- Kurs-1/84.js
+++ Kurs-1/84.js
@@ -1,4 +1,42 @@
 const wallet = {
     balance: 0,
     operations: [],
+    colOperation: 0,
+    upBalance(reason, sum) {
+        this.operations.push({ reason: reason, sum });
+        this.balance += sum;
+        this.colOperation++;
+        console.log(this.operations);
+        console.log(this.balance);
+        return true;
+    },
+    downBalance(reason, sum) {
+        this.colOperation++;
+        if (this.balance >= sum) {
+            this.operations.push({
+                reason,
+                sum: -sum,
+            });
+            console.log(this.operations);
+            console.log(this.balance);
+            this.balance -= sum;
+            return true;
+        } else {
+            console.log("недостаточно средств");
+            return false;
+        }
+    },
+    getColOperation() {
+        console.log(this.operations.length);
+        return this.operations.length;
+        console.log(`Текущий баланс ${this.balance}`);
+        console.log(this.colOperation);
+    },
 };
+
+wallet.upBalance("Пополнение баланса", 1000);
+wallet.upBalance("Пополнение баланса", 2000);
+wallet.upBalance("Пополнение баланса", 3000);
+wallet.downBalance("Оплата КУ", 2700);
+wallet.downBalance("Оплата Подписки", 3700);
+wallet.getColOperation();
---
+++ Kurs-1/89.js
@@ -0,0 +1,63 @@
+const warehouse = {
+    goods: [],
+    findGoodById: function (id) {
+        // console.log(
+        //     "Товар найлен: ",
+        //     this.goods.find((g) => g.id == id)
+        // );
+        return this.goods.find((g) => g.id == id);
+        // const existedGood = this.goods.find((g) => g.id == id);
+        // return console.log(existedGood);
+    },
+    addGood(good) {
+        if (this.findGoodById(good.id)) {
+            console.log("Этот тавар уже есть на скалде", good);
+            return;
+        }
+        // const existedGood = this.goods.find((g) => g.id == good.id);
+        // if (existedGood) {
+        //     console.log("Этот тавар уже есть на скалде");
+        //     return;
+        // }
+        this.goods.push(good);
+    },
+    getWidthKg(obg) {
+        return this.goods.reduce(
+            (acc, el) => (acc += el.weigth?.kg ? el.weigth.kg : 0),
+            0
+        );
+    },
+};
+
+const car = {
+    id: 1,
+    weigth: {
+        kg: 1000,
+    },
+    brand: "Ford",
+};
+
+const chair = {
+    id: 2,
+    weigth: {
+        kg: 2,
+    },
+};
+
+const paper = {
+    id: 3,
+    color: "red",
+};
+
+//find()
+//sum reduse
+
+warehouse.addGood(car);
+warehouse.addGood(car);
+warehouse.addGood(chair);
+warehouse.addGood(paper);
+console.log(warehouse.goods);
+console.log("Найден товар: ", warehouse.findGoodById(1));
+console.log("Найден товар: ", warehouse.findGoodById(2));
+console.log("Найден товар: ", warehouse.findGoodById(3));
+console.log("Общий вес: ", warehouse.getWidthKg());
