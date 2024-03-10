4846c307771ccf798a76c9f04d1aedd0d28106a3
Artyom.Evsyukov <artyom.evsyukov@gmail.com>
2024-03-10 11:22:59 +0300

    work 10.03

--- Learn.JS/getters-setters.js
+++ Learn.JS/getters-setters.js
@@ -106,21 +106,184 @@
 // console.log(user.name); // Alice
 // console.log(user.surname); // Cooper
 // console.log(user.fullName);
-let user = {};
+// let user = {};
 
-Object.defineProperty(user, "name", {
-    value: "John",
-    // для нового свойства необходимо явно указывать все флаги, для которых значение true
-    // writable: true,
-    enumerable: true,
-    configurable: true,
-});
-console.log(Object.getOwnPropertyDescriptor(user, "name"));
-Object.defineProperty(user, "name", {
-    writable: true,
-});
-console.log(user.name); // John
-user.name = "Pete"; // Ошибка
-console.log(Object.getOwnPropertyDescriptor(user, "name", 2));
-console.log(user.name); // John
+// Object.defineProperty(user, "name", {
+//     value: "John",
+//     // для нового свойства необходимо явно указывать все флаги, для которых значение true
+//     // writable: true,
+//     enumerable: true,
+//     configurable: true,
+// });
+// console.log(Object.getOwnPropertyDescriptor(user, "name"));
+// Object.defineProperty(user, "name", {
+//     writable: true,
+// });
+// console.log(user.name); // John
 // user.name = "Pete"; // Ошибка
+// console.log(Object.getOwnPropertyDescriptor(user, "name", 2));
+// console.log(user.name); // John
+// // user.name = "Pete"; // Ошибка
+
+// let user = {
+//     name: "John",
+//     age: 22,
+// };
+// console.log(user.name);
+
+// Object.defineProperty(user, "name", {
+//     writable: false,
+//     enumerable: false,
+//     configurable: false,
+// });
+// Object.defineProperty(user, "name", {
+//     // writable: true,
+//     writable: false,
+//     enumerable: false,
+//     configurable: true,
+// });
+
+// let descriptor = Object.getOwnPropertyDescriptor(user, "name");
+// console.log(JSON.stringify(descriptor, null, 2));
+
+// Object.defineProperty(user, "name", { value: "Pete" });
+// user.name = "Pete";
+// console.log(user);
+// console.log(user.name);
+// console.log(JSON.stringify(user, null, 2));
+// console.log(
+//     JSON.stringify(
+//         user,
+//         function (key, value) {
+//             if (typeof value == "string") {
+//                 return undefined;
+//             } else return value;
+//         },
+//         2
+//     )
+// );
+
+// let user = {
+//     name: "John",
+//     surname: "Smith",
+
+//     get fullName() {
+//         return `${this.name} ${this.surname}`;
+//     },
+
+//     set fullName(value) {
+//         [this.name, this.surname] = value.split(" ");
+//     },
+// };
+// // set fullName запустится с данным значением
+// user.fullName = "Alice Cooper";
+
+// // Object.defineProperty(user, "fullName", { configurable: false });
+// // delete user.fullName;
+
+// console.log(user.name); // Alice
+// console.log(user.surname); // Cooper
+// console.log(user.fullName);
+// =================================================
+// let user = {
+//     name: "John",
+//     surname: "Smith",
+// };
+
+// Object.defineProperty(user, "fullName", {
+//     get() {
+//         return `${this.name} ${this.surname}`;
+//     },
+
+//     set(value) {
+//         [this.name, this.surname] = value.split(" ");
+//     },
+// });
+
+// // delete user.fullName;
+
+// console.log(user.fullName); // John Smith
+// user.fullName = "Alice Cooper";
+
+// for (let key in user) console.log(key); // name, surname
+
+// // let clone = Object.defineProperties(user);
+// console.log(Object.getOwnPropertyDescriptors(user));
+// // let clone2 = Object.getOwnPropertyDescriptors(user);
+// // console.log(clone2);
+// console.log(user.fullName); // John Smith
+// ====================================================================================
+// let user = {
+//     get name() {
+//         return this._name;
+//     },
+
+//     set name(value) {
+//         if (value.length < 4) {
+//             console.log("Имя слишком короткое, должно быть более 4 символов");
+//             return;
+//         }
+//         this._name = value;
+//     },
+// };
+
+// user.name = "Pete";
+// console.log(user.name); // Pete
+
+// user.name = ""; // Имя слишком короткое...
+// ====================================================================================
+
+// let user = {
+//     get name() {
+//         return this._name;
+//     },
+//     set name(value) {
+//         if (value.length < 4) {
+//             console.log("Слишком короткое имя, должно быть больше 4 символов");
+//             return;
+//         }
+//         this._name = value;
+//     },
+// };
+
+// user.name = "Pete";
+// console.log(user.name);
+// user.name = "Pe";
+// ====================================================================================
+
+// function User(name, birthday) {
+//     this.name = name;
+//     this.birthday = birthday;
+//     // this.age = age;
+//     Object.defineProperty(this, "age", {
+//         get() {
+//             let todayYear = new Date().getFullYear();
+//             return todayYear - this.birthday.getFullYear();
+//         },
+//     });
+// }
+
+// let john = new User("John", new Date(1992, 6, 1));
+
+// console.log(john.birthday); // 25
+// console.log(john.age); // 25
+
+// ====================================================================================
+
+function User(name, birthday) {
+    this.name = name;
+    this.birthday = birthday;
+
+    Object.defineProperty(this, "age", {
+        get() {
+            let todayYear = new Date().getFullYear();
+            return todayYear - this.birthday.getFullYear();
+        },
+    });
+}
+// let John = new User("John", 25);
+let John = new User("John", new Date(1987, 22, 8));
+console.log(John);
+console.log(John.name);
+console.log(John.birthday);
+console.log(John.age);
