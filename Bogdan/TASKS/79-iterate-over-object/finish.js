65a50db06ac4ce5f158a6a2ba2c2cfb82b9d4a06
Artyom.Evsyukov <artyom.evsyukov@gmail.com>
2024-03-21 15:32:32 +0300

    work 21.03/3

--- Bogdan/TASKS/79-iterate-over-object/finish.js
+++ Bogdan/TASKS/79-iterate-over-object/finish.js
@@ -10,14 +10,42 @@
  */
 
 const objectWithNumbers = {
-  a: 10,
-  b: 20,
-  c: 'string',
-  d: 12,
+    a: 10,
+    b: 20,
+    c: "string",
+    d: 12,
+};
+
+// function sumObjectValues(obj) {
+//     let sum = 0;
+//     for (const [key, value] of Object.entries(obj)) {
+//         obj.hasOwnProperty(key) && typeof value === "number"
+//             ? (sum += value)
+//             : sum;
+//     }
+//     return sum;
+// }
+// function sumObjectValues(nums) {
+//     let sum = 0;
+
+//     console.log(Object.keys(nums));
+//     Object.keys(nums).forEach((value) => {
+//         if (typeof nums[value] === "number") {
+//             sum += nums[value];
+//         }
+//     });
+//     return sum;
+// }
+function sumObjectValues(nums) {
+    console.log(Object.values(nums));
+    return Object.values(nums).reduce((acc, value) => {
+        if (typeof value === "number") {
+            return acc + value;
+        }
+        return acc;
+    }, 0);
 }
 
-// Создайте функцию здесь
-
-const result = sumObjectValues(objectWithNumbers)
-console.log(result)
+const result = sumObjectValues(objectWithNumbers);
+console.log(result);
 //42
