bc8634f5660230c6d3dd84c99c867117696adeb3
Artyom.Evsyukov <artyom.evsyukov@gmail.com>
2024-04-15 12:25:44 +0300

    work 15.04

--- Kurs-1/124.js
+++ Kurs-1/124.js
@@ -1,31 +1,5 @@
 "use strict";
 
-// /* key: 'text', value: { text: 'sdfsdf' } */
-
-// function submitForm() {
-//     const input = document.querySelector(".input").value;
-//     if (!input) {
-//         return;
-//     }
-//     document.querySelector(".panel").innerText = input;
-//     document.querySelector(".input").value = "";
-//     document
-//         .querySelector(".notification")
-//         .classList.remove("notification_hidden");
-//     const textString = JSON.stringify({
-//         text: input,
-//     });
-//     localStorage.setItem("text", textString);
-// }
-
-// function inputChanged(e) {
-//     if (e.code == "Enter") {
-//         submitForm();
-//     }
-// }
-
-"use strict";
-
 /* key: 'text', value: { text: 'sdfsdf' } */
 
 function submitForm() {
@@ -38,12 +12,10 @@ function submitForm() {
     document
         .querySelector(".notification")
         .classList.remove("notification_hidden");
-
-    let valueObj = {
+    const textString = JSON.stringify({
         text: input,
-    };
-    console.log(valueObj.text);
-    // const textString =
+    });
+    localStorage.setItem("text", textString);
 }
 
 function inputChanged(e) {
