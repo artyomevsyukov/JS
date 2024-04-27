9700eab798e55a3769b79e845bc184de1c9bf537
Artyom.Evsyukov <artyom.evsyukov@gmail.com>
2024-04-27 14:18:35 +0300

    work 27.04

---
+++ Kurs-2/65.js
@@ -0,0 +1,44 @@
+// function pizzaTimer(ms) {
+//     ms += 1;
+//     const interval = setInterval(function go() {
+//         ms -= 1;
+//         ms < 10 ? console.log(`00:0${ms}`) : console.log(`00:${ms}`);
+//         if (ms <= 0) {
+//             clearInterval(interval);
+//             console.log("Ура! Ваша пицца готова!");
+//         }
+//     }, 200);
+// }
+
+// function pizzaTimer(ms) {
+//     // ms *= 1000;
+//     const end = new Date().getTime() + ms;
+//     const interval = setInterval(() => {
+//         console.log(
+//             new Intl.DateTimeFormat("ru-RU", {
+//                 minute: "numeric",
+//                 second: "numeric",
+//             }).format(end + 100 - new Date().getTime())
+//         );
+//     }, 1000);
+//     setTimeout(() => {
+//         clearInterval(interval);
+//         console.log("Пицца готова!");
+//     }, ms);
+// }
+function pizzaTimer(ms) {
+    const end = new Date().getTime() + ms;
+    const interval = setInterval(() => {
+        console.log(
+            new Intl.DateTimeFormat("ru-RU", {
+                minute: "numeric",
+                second: "numeric",
+            }).format(end + 100 - new Date())
+        );
+    }, 1000);
+    setTimeout(() => {
+        clearInterval(interval);
+        console.log("Ваша пицца готова!!!");
+    }, ms);
+}
+pizzaTimer(500000);
--- Kurs-2/test.js
+++ Kurs-2/test.js
@@ -3,3 +3,4 @@ const now = new Date();
 console.log(now.getDay());
 console.log(new Date().getDay());
 console.log(new Date().getDate());
+console.log(performance.now());
