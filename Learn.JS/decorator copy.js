4d8dfdb9069940bf95fa96aae159a9ae465714be
Artyom.Evsyukov <artyom.evsyukov@gmail.com>
2024-05-16 21:49:58 +0300

    home 16.05

--- Learn.JS/decorator copy.js
+++ Learn.JS/decorator copy.js
@@ -13,13 +13,52 @@
 // for (let args of work.calls) {
 //     console.log("call:" + args.join()); // "call:1,2", "call:4,5"
 // }
-function exampleFunction() {
-    console.log(this);
-}
-let contextObject = { key: "value" };
-exampleFunction.call(contextObject); // this будет указывать на contextObject
+// function exampleFunction() {
+//     console.log(this);
+// }
+// let contextObject = { key: "value" };
+// exampleFunction.call(contextObject); // this будет указывать на contextObject
 
-let arrowFunction = () => {
-    console.log(this);
-};
-arrowFunction(); // this будет зависеть от контекста, в котором определена arrowFunction
+// let arrowFunction = () => {
+//     console.log(this);
+// };
+// arrowFunction(); // this будет зависеть от контекста, в котором определена arrowFunction
+
+function work(a, b) {
+    console.log(a + b); // произвольная функция или метод
+}
+
+// function spy(func) {
+//     wrapper.calls = [];
+//     function wrapper(...args) {
+//         wrapper.calls.push(args);
+//         return func(...args);
+//     }
+//     return wrapper;
+// }
+
+// function spy(func) {
+//     return function wrapper(...args) {
+//         wrapper.calls = [];
+//         wrapper.calls.push(args);
+//         return func(...args);
+//     };
+// }
+
+// function spy(func) {
+//     wrapper.calls = [];
+//     function wrapper(...args) {
+//         wrapper.calls.push(args);
+//         return func(...args);
+//     }
+//     return wrapper;
+// }
+
+// work = spy(work);
+
+// work(1, 2); // 3
+// work(4, 5); // 9
+
+// for (let args of work.calls) {
+//     console.log("call:" + args.join()); // "call:1,2", "call:4,5"
+// }
