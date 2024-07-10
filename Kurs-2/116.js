6de6d86e9eb7839e4a88111de0fa1d8ee15e4548
Artyom.Evsyukov <artyom.evsyukov@gmail.com>
2024-07-10 15:10:00 +0300

    work 10.07

---
+++ Kurs-2/116.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const request = new XMLHttpRequest();
+request.open("GET", "https://dummyjson.com/products/");
+request.send();
+
+request.addEventListener("load", function () {
+    const { products } = JSON.parse(this.responseText);
+
+    const sum = products.reduce((acc, product) => ((acc += product.price), 0));
+    console.log(products);
+    console.log(sum);
+});
