f9af3c8f4d8b383b09c5355bdaa23085018869d3
Artyom.Evsyukov <artyom.evsyukov@gmail.com>
2024-07-12 14:32:21 +0300

    work 12.07

---
+++ Learn.JS/Промисы, async/promise-basics.js
@@ -0,0 +1,7 @@
+// function delay(ms) {
+//     return new Promise((resolve) => setTimeout(() => resolve("done"), ms));
+// }
+
+// delay(1000)
+//     .then((text) => console.log("выполнилось через 3 секунды ", text))
+//     .catch((e) => console.log(e.message));
