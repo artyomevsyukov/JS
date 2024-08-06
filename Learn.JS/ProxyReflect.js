0f31b0cbd7bf8fc9f3e2267edc35f6a42ed2a279
Artyom.Evsyukov <artyom.evsyukov@gmail.com>
2024-08-06 15:05:10 +0300

    work 06.08

--- Learn.JS/ProxyReflect.js
+++ Learn.JS/ProxyReflect.js
@@ -1,16 +1,18 @@
-// let numbers = [0, 1, 2];
+// let numbers = [1, 2, 3];
 
 // numbers = new Proxy(numbers, {
 //     get(target, prop) {
-//         if (prop in target) {
-//             return target[prop];
-//         } else {
-//             return 0; // значение по умолчанию
-//         }
+//         // if (prop in target) {
+//         //     return target[prop];
+//         // } else {
+//         //     return 0; // значение по умолчанию
+//         // }
+
+//         return prop in target ? target[prop] : 0;
 //     },
 // });
 
-// console.log(numbers[1]); // 1
+// console.log(numbers[1]); // 2
 // console.log(numbers[123]); // 0 (нет такого элемента)
 
 // let dictionary = {
@@ -31,21 +33,66 @@
 // console.log(dictionary["Hello"]); // Hola
 // console.log(dictionary["Welcome to Proxy"]); // Welcome to Proxy (нет перевода)
 
-let numbers = [];
+// let numbers = [];
 
-numbers = new Proxy(numbers, {
+// numbers = new Proxy(numbers, {
+//     set(target, prop, val) {
+//         if (typeof val == "number") {
+//             target[prop] = val;
+//             return true;
+//         } else {
+//             return false;
+//         }
+//     },
+// });
+
+// numbers.push(1); // добавилось успешно
+// numbers.push(2); // добавилось успешно
+// console.log("Длина: " + numbers.length); // 2
+
+// numbers.push("тест"); // TypeError (ловушка set на прокси вернула false)
+
+// let dictionary = {
+//     Hello: "Hola",
+//     Bye: "Adiós",
+// };
+
+// let proxyHendler = {
+//     get(target, phrase) {
+//         return phrase in target ? target[phrase] : phrase;
+//     },
+// };
+
+// dictionary = new Proxy(dictionary, proxyHendler);
+
+// console.log(dictionary["Hello"]);
+// console.log(dictionary["Bye"]);
+// console.log(dictionary["bye"]);
+
+let number = [];
+
+number = new Proxy(number, {
     set(target, prop, val) {
         if (typeof val == "number") {
             target[prop] = val;
             return true;
         } else {
-            return false;
+            // return false;
+            throw new TypeError(
+                `Недопустимое значение: ${val} не является числом`
+            );
         }
     },
 });
 
-numbers.push(1); // добавилось успешно
-numbers.push(2); // добавилось успешно
-console.log("Длина: " + numbers.length); // 2
+const valuesToAdd = [1, "vbncvn", 2, 3, "sdff", 4, 5];
 
-// numbers.push("тест"); // TypeError (ловушка set на прокси вернула false)
+valuesToAdd.forEach((value) => {
+    try {
+        number.push(value);
+    } catch (error) {
+        console.error(error.message);
+    }
+});
+
+console.log(number);
