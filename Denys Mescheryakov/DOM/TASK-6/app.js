070bbf157ef2374fce26237d0e4cb80c77b825b6
Artyom.Evsyukov <artyom.evsyukov@gmail.com>
2024-09-28 12:51:09 +0300

    TABLE dz

---
+++ Denys Mescheryakov/DOM/TASK-6/app.js
@@ -0,0 +1,55 @@
+const users = [
+    {
+        id: "t624654645747",
+        name: "John Smith",
+        age: 20,
+        balance: 2853.33,
+        gender: "male",
+        email: "sdgsgg@gfsgfd.com",
+        phone: "1564861234",
+        company: "KNKLCV",
+    },
+    {
+        id: "h452674747567",
+        name: "Ann Smith",
+        age: 24,
+        balance: 23422.12,
+        gender: "male",
+        email: "jklhjkl@bnmbnm.com",
+        phone: "1564861234",
+        company: "KNKLCV",
+    },
+    {
+        id: "j315346758676",
+        name: "Pete Peterson",
+        age: 31,
+        balance: 6575.64,
+        gender: "male",
+        email: "lkjukj@qerter.com",
+        phone: "1564861234",
+        company: "KNKLCV",
+    },
+    {
+        id: "l345345345311",
+        name: "Max Hash",
+        age: 34,
+        balance: 8797,
+        gender: "male",
+        email: "zxcvdfgs@awefw.com",
+        phone: "1564861234",
+        company: "KNKLCV",
+    },
+];
+
+const tableSchema = {
+    index: "#",
+    name: "Name",
+    email: "Email",
+    balance: "Balance",
+};
+
+// generateThead
+// generateTbody
+// generateTr
+// generateTableTemplate
+// initTable
