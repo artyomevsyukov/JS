ce77b7540f06ec75d33ddcaa72688900f049eb82
Artyom.Evsyukov <artyom.evsyukov@gmail.com>
2024-09-30 14:41:02 +0300

    Дз создание таймера

---
+++ Иван Петриченко/64_food/js/main.js
@@ -0,0 +1,45 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+    // Tabs 64 =============================================================================
+    const tabs = document.querySelectorAll(".tabheader__item");
+    const tabsContent = document.querySelectorAll(".tabcontent");
+    const tabsParent = document.querySelector(".tabheader__items");
+
+    function hideTabContent() {
+        tabsContent.forEach((item) => {
+            // item.style.display = "none";
+            item.classList.add("hide");
+            item.classList.remove("show", "fade");
+        });
+
+        tabs.forEach((item) => {
+            item.classList.remove("tabheader__item_active");
+        });
+    }
+    function showTabsContent(i = 0) {
+        // tabsContent[i].style.display = "block";
+        tabsContent[i].classList.add("show", "fade");
+        tabsContent[i].classList.remove("hide");
+        tabs[i].classList.add("tabheader__item_active");
+    }
+
+    hideTabContent();
+    showTabsContent();
+
+    tabsParent.addEventListener("click", (event) => {
+        const { target } = event;
+        if (target && target.classList.contains("tabheader__item")) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabsContent(i);
+                }
+            });
+        }
+    });
+
+    // Timer
+    // Tabs 69 =============================================================================
+    const dedline = "2024-10-11";
+});
