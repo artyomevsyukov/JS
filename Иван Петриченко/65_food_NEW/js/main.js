7834b7eea92f32589aafd0f05f3811847bd144e6
Artyom.Evsyukov <artyom.evsyukov@gmail.com>
2024-10-05 10:32:26 +0300

    Добавил получение информации для калькулятора

--- Иван Петриченко/65_food_NEW/js/main.js
+++ Иван Петриченко/65_food_NEW/js/main.js
@@ -596,25 +596,77 @@ document.addEventListener("DOMContentLoaded", () => {
     // Calc =========================================================
 
     const result = document.querySelector(".calculating__result span");
-    let sex, heigth, weight, age, ratio;
+    const gender = document.querySelector("#gender");
+    const activity = document.querySelector(".calculating__choose_big");
+
+    let sex, ratio;
+    let height, weight, age;
 
     function calcTotal() {
-        if (!sex || !heigth || !weight || !age || !ratio) {
+        if (!sex || !height || !weight || !age || !ratio) {
             result.textContent = "ОШИБКА";
             return;
         }
         if (sex == "famale") {
-            result.textContent =
-                (447.6 + 9.2 * weight + 3.1 * heigth - 4.3 * age) * ratio;
+            result.textContent = Math.floor(
+                (447.6 + 9.2 * weight + 3.1 * height - 4.3 * age) * ratio
+            );
         } else {
-            result.textContent =
-                (88.36 + 13.4 * weight + 4.8 * heigth - 5.7 * age) * ratio;
+            result.textContent = Math.floor(
+                (88.36 + 13.4 * weight + 4.8 * height - 5.7 * age) * ratio
+            );
         }
     }
 
-    calcTotal();
-
     function getStaticInformation(parentSelector, activeClass) {
-        const element = document.querySelectorAll(`${parentSelector} div`);
+        const parentElement = document.querySelector(parentSelector);
+        const elements = document.querySelectorAll(`${parentSelector} div`);
+
+        parentElement.addEventListener("click", (e) => {
+            if (!e.target.classList.contains("calculating__choose-item"))
+                return;
+
+            if (e.target.getAttribute("data-ratio")) {
+                ratio = +e.target.getAttribute("data-ratio");
+                console.log(ratio);
+            } else {
+                sex = e.target.getAttribute("id");
+                console.log(sex);
+            }
+
+            elements.forEach((elem) => {
+                elem.classList.remove(activeClass);
+            });
+            e.target.classList.add(activeClass);
+            calcTotal();
+        });
     }
+
+    getStaticInformation("#gender", "calculating__choose-item_active");
+    getStaticInformation(
+        ".calculating__choose_big",
+        "calculating__choose-item_active"
+    );
+
+    // <input type="text" id="height" placeholder="Введите рост" class="calculating__choose-item">
+    // <input type="text" id="weight" placeholder="Введите вес" class="calculating__choose-item">
+    // <input type="text" id="age" placeholder="Введите возраст" class="calculating__choose-item">
+    // const obj = {};
+    function getDinamicInformation(selector) {
+        const element = document.querySelectorAll(`${selector} input`);
+        const obj = {};
+        element.forEach((el) => {
+            el.addEventListener("input", () => {
+                let id = el.id;
+                obj[id] = el.value;
+                console.log(obj);
+                ({ height, weight, age } = obj);
+                console.log("height, weight, age ", height, weight, age);
+                calcTotal();
+            });
+        });
+    }
+
+    getDinamicInformation(".calculating__choose");
+    calcTotal();
 });
