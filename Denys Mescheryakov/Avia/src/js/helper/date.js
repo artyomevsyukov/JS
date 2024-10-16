207a734c185c2ffcaf2a515aed0d8540abb244d1
Artyom.Evsyukov <artyom.evsyukov@gmail.com>
2024-10-16 22:18:35 +0300

    JSDoc-аннотация

--- Denys Mescheryakov/Avia/src/js/helper/date.js
+++ Denys Mescheryakov/Avia/src/js/helper/date.js
@@ -2,9 +2,9 @@ import { format } from "date-fns";
 
 /**
  *
- * @param {String} str // Дата в виде строки
- * @param {String} type
- * @returns
+ * @param {String} str Дата в виде строки
+ * @param {String} type Формат даты (например, 'yyyy-mm-dd' или 'dd-mm-yyyy')
+ * @returns {String} Преобразованная дата
  */
 export function formatDate(str, type) {
     const date = new Date(str);
