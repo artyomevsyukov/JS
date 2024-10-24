25c7619e480830175f8bc861a6c3f89c66d397be
Artyom.Evsyukov <artyom.evsyukov@gmail.com>
2024-10-24 14:26:12 +0300

    home 24.10

--- test4.js
+++ test4.js
@@ -1,8 +1,24 @@
-const obj = {
-    title: "Conference",
-    room: 23,
+// const obj = {
+//     title: "Conference",
+//     room: 23,
+// };
+
+// console.log(obj);
+
+// console.log(JSON.stringify(obj));
+
+const uniqueInOrder = function (iterable) {
+    const newArr = [];
+    let marker = "";
+    for (const el of iterable) {
+        if (el !== marker) {
+            newArr.push(el);
+            marker = el; // Обновляем marker на текущий элемент
+        }
+    }
+    return newArr;
 };
 
-console.log(obj);
-
-console.log(JSON.stringify(obj));
+console.log(uniqueInOrder("AAAABBBCCDAABBB")); // ["A", "B", "C", "D", "A", "B"];
+console.log(uniqueInOrder("ABBCcAD")); // ["A", "B", "C", "c", "A", "D"];
+console.log(uniqueInOrder([1, 2, 2, 3, 3])); // [1, 2, 3];
