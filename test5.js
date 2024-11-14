5a4dd65c225e5ebdf7ca8e2e6f041f812dbe4120
Artyom.Evsyukov <artyom.evsyukov@gmail.com>
2024-11-14 14:37:58 +0300

    Canvas 2

--- test5.js
+++ test5.js
@@ -56,57 +56,62 @@
 
 // console.log(city.name);
 
-const key = "204-61030";
+// const key = "204-61030";
 
-const tickets = [
-    {
-        origin: "LED",
-        destination: "MOW",
-        airline: "N4",
-        departure_at: "07 Nov 2024 18:10",
-        return_at: "28 Nov 2024 17:25",
-        expires_at: "2024-11-07T08:17:10Z",
-        price: 4622,
-        flight_number: 204,
-        transfers: 0,
-        key: "204-50182",
-        origin_name: "Санкт-Петербург",
-        destination_name: "Москва",
-        airline_logo: "https://pics.avs.io/200/200/N4.png",
-        airline_name: "Nordwind Airlines",
-    },
-    {
-        origin: "LED",
-        destination: "MOW",
-        airline: "N4",
-        departure_at: "08 Nov 2024 21:10",
-        return_at: "28 Nov 2024 19:10",
-        expires_at: "2024-11-07T08:17:10Z",
-        price: 4761,
-        flight_number: 204,
-        transfers: 0,
-        key: "204-61030",
-        origin_name: "Санкт-Петербург",
-        destination_name: "Москва",
-        airline_logo: "https://pics.avs.io/200/200/N4.png",
-        airline_name: "Nordwind Airlines",
-    },
-    {
-        origin: "LED",
-        destination: "MOW",
-        airline: "UT",
-        departure_at: "09 Nov 2024 21:30",
-        return_at: "11 Nov 2024 19:35",
-        expires_at: "2024-11-07T08:17:10Z",
-        price: 4651,
-        flight_number: 382,
-        transfers: 0,
-        key: "382-43983",
-        origin_name: "Санкт-Петербург",
-        destination_name: "Москва",
-        airline_logo: "https://pics.avs.io/200/200/UT.png",
-        airline_name: "Utair",
-    },
-];
+// const tickets = [
+//     {
+//         origin: "LED",
+//         destination: "MOW",
+//         airline: "N4",
+//         departure_at: "07 Nov 2024 18:10",
+//         return_at: "28 Nov 2024 17:25",
+//         expires_at: "2024-11-07T08:17:10Z",
+//         price: 4622,
+//         flight_number: 204,
+//         transfers: 0,
+//         key: "204-50182",
+//         origin_name: "Санкт-Петербург",
+//         destination_name: "Москва",
+//         airline_logo: "https://pics.avs.io/200/200/N4.png",
+//         airline_name: "Nordwind Airlines",
+//     },
+//     {
+//         origin: "LED",
+//         destination: "MOW",
+//         airline: "N4",
+//         departure_at: "08 Nov 2024 21:10",
+//         return_at: "28 Nov 2024 19:10",
+//         expires_at: "2024-11-07T08:17:10Z",
+//         price: 4761,
+//         flight_number: 204,
+//         transfers: 0,
+//         key: "204-61030",
+//         origin_name: "Санкт-Петербург",
+//         destination_name: "Москва",
+//         airline_logo: "https://pics.avs.io/200/200/N4.png",
+//         airline_name: "Nordwind Airlines",
+//     },
+//     {
+//         origin: "LED",
+//         destination: "MOW",
+//         airline: "UT",
+//         departure_at: "09 Nov 2024 21:30",
+//         return_at: "11 Nov 2024 19:35",
+//         expires_at: "2024-11-07T08:17:10Z",
+//         price: 4651,
+//         flight_number: 382,
+//         transfers: 0,
+//         key: "382-43983",
+//         origin_name: "Санкт-Петербург",
+//         destination_name: "Москва",
+//         airline_logo: "https://pics.avs.io/200/200/UT.png",
+//         airline_name: "Utair",
+//     },
+// ];
 
-console.log(tickets.find((ticket) => ticket.key === key));
+// console.log(tickets.find((ticket) => ticket.key === key));
+
+const numbers = [1, 2, 3, 4];
+const doubled = numbers.map((num) => num * 2);
+console.log(doubled); // [2, 4, 6, 8]
+console.log(numbers); // [1, 2, 3, 4] (исходный массив не изменен)
